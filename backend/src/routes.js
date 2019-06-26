45ecae7e507f61f5759773408c5ef79c2f637cb8
mjlbnu <mjlbnu@gmail.com>
2019-06-25 22:05:59 -0300

    rotas adicionadas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const multer = require('multer')
+const uploadConfig = require('./config/upload')
+
+const PostController = require('./controllers/PostController')
+const LikeController = require('./controllers/LikeController')
 
 const routes = new express.Router()
+const upload = multer(uploadConfig)
 
 // intercept
-routes.get('/', (req, res) => {
-    return res.send(`Olá ${req.query.name}`)
+routes.get('/posts', PostController.index)
+routes.post('/posts', upload.single('image'), PostController.store)
 
-})
+routes.post('/posts/:id/like', LikeController.store)
 
 module.exports = routes
